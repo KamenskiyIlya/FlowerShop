84f71b7396ccf3e25a229393e7853a1a42b62241
Aleksandr Babynin <babynin.home@gmail.com>
2026-04-25 23:40:57 +1100

    добавил редирект с корня сайта на /admin/ чтобы пользователя не пугала ошибка при открытии 127.0.0.1:8000/

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
